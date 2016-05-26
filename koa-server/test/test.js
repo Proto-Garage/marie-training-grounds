75b3601187365b4d3f27078618c1d600c9929ff8
Rienette <swit.macg29@gmail.com>
2016-05-27 02:55:40 +0800

    Add Bare Server Test Files

---
+++ koa-server/test/test.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+
+describe('Task Manager', function(){
+  describe('Create POST /task', function(){
+
+  });
+
+  describe('Retrieve GET /task', function(){
+
+  });
+
+  describe('Update PUT /task/:id', function(){
+
+  });
+
+  describe('Delete DEL /task/:id', function(){
+
+  });
+});
